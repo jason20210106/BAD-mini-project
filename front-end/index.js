4e2f139a3df2ed41e2f9f6cae2399a74eaa75d74
Jason Yim <jason20210106@gmail.com>
2023-11-17 21:36:37 +0800

    front end

--- front-end/index.js
+++ front-end/index.js
@@ -18,34 +18,52 @@
 //     box3.textContent = "GOOD MORNING";
 //   }
 // }
-
 let clickCount = 0;
 
-
-// const addTaskBtn = document.querySelector("#addTaskBtn");
-// addTaskBtn.addEventListener("click", myFunction);
-
 const taskInput = document.querySelector("#taskInput");
+const addTaskBtn = document.querySelector("#addTaskBtn"); // Add the missing declaration for the "addTaskBtn"
+const reload = document.querySelector("#reload")
+
 addTaskBtn.addEventListener("click", myFunction);
+
 function myFunction() {
   const box1 = document.querySelector(".box1");
   const box2 = document.querySelector(".box2");
   const box3 = document.querySelector(".box3");
+  const box4 = document.querySelector(".box4");
+  const box5 = document.querySelector(".box5");
+  const box6 = document.querySelector(".box6");
+  const box7 = document.querySelector(".box7");
 
   clickCount++;
 
   if (clickCount === 1) {
     box1.textContent = taskInput.value;
-    taskInput.value = ""
+    taskInput.value = "";
   } else if (clickCount === 2) {
     box2.textContent = taskInput.value;
-    taskInput.value = ""
+    taskInput.value = "";
   } else if (clickCount === 3) {
     box3.textContent = taskInput.value;
-    taskInput.value = ""
+    taskInput.value = "";
+  } else if (clickCount === 4) {
+    box4.textContent = taskInput.value;
+    taskInput.value = "";
+  } else if (clickCount === 5) {
+    box5.textContent = taskInput.value;
+    taskInput.value = "";
+  } else if (clickCount === 6) {
+    box6.textContent = taskInput.value;
+    taskInput.value = "";
+  } else if (clickCount === 7) {
+    box7.textContent = taskInput.value;
+    taskInput.value = "";
   }
 }
 
+reload.addEventListener("click",function(){
+    window.location.reload();
+})
     
 
 // const addTaskBtn1 = document.querySelector("#addTaskBtn")
